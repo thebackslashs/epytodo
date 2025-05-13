5a20a89dd47afc7145d9b1ed09a342177fbb1df8
Dimitri Collineau <dimitri.collineau@epitech.eu>
2025-05-13 16:25:17 +0200

    feat(create a todo): add CreateTodoDTO for validation and refactor createTodo method in TodoService

--- src/modules/todo/controllers/todo.controller.ts
+++ src/modules/todo/controllers/todo.controller.ts
@@ -3,7 +3,7 @@ import { Request } from 'express';
 import { TodoService } from '../services/todo.service';
 import { Middleware } from '@/core/decorators/middleware.decorator';
 import ValidatorMiddleware from '@/middlewares/validator.middleware';
-import CreateTodoDTO, { InferCreateTodoDTO } from '../dtos/create-todo.dto';
+import CreateTodoDTO, { InferCreateTodoDTO } from './../dtos/create-todo.dto';
 import { Todo } from '../models/todo.model';
 
 @Controller('/todos')
@@ -14,10 +14,7 @@ export class TodoController {
     @Middleware(ValidatorMiddleware(CreateTodoDTO))
     async createTodo(req: Request): Promise<{ todo: Todo }> {
         const todoData = req.body as InferCreateTodoDTO;
-        const todo = await this.todoService.createTodo({
-            ...todoData,
-            due_time: new Date(todoData.due_time)
-        });
+        const todo = await this.todoService.createTodo(todoData);
         return { todo };
     }
 }
--- src/modules/todo/services/todo.service.ts
+++ src/modules/todo/services/todo.service.ts
@@ -1,12 +1,17 @@
 import { Inject, Injectable } from '@/core';
 import { TodoRepo } from '../repos/todo.repo';
 import { Todo } from '../models/todo.model';
+import { InferCreateTodoDTO } from './../dtos/create-todo.dto';
 
 @Injectable()
 export class TodoService {
   constructor(@Inject('TodoRepo') private readonly todoRepo: TodoRepo) {}
 
-  async createTodo(todoData: Omit<Todo, 'id' | 'created_at'>): Promise<Todo> {
-    return this.todoRepo.create(todoData);
+  async createTodo(todoData: InferCreateTodoDTO): Promise<Todo> {
+    const parsedData = {
+      ...todoData,
+      due_time: new Date(todoData.due_time)
+    };
+    return this.todoRepo.create(parsedData);
   }
 }
