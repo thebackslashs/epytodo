df687e2bece635ae27258530c889cc4e7b07b93b
Ely Delva <ely.delva@outlook.com>
2025-05-13 02:35:53 +0200

    refactor(user): update created_at type to match date format
    * Change created_at type in User model from Date to string for consistency
    * Modify UserService methods to format created_at date when retrieving users
    * Ensure proper async handling in createUser and updateUser methods

--- src/modules/user/models/user.model.ts
+++ src/modules/user/models/user.model.ts
@@ -4,5 +4,5 @@ export interface User {
   firstname: string;
   email: string;
   password: string;
-  created_at: Date;
+  created_at: string;
 }
--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,25 +1,34 @@
 import { User } from '@/modules/user/models/user.model';
 import UserRepo from '../repos/user.repo';
 import { Inject, Injectable } from '@/core';
+import { formatDate } from '@/lib/dates';
 
 @Injectable()
 export class UserService {
   constructor(@Inject('UserRepo') private readonly userRepo: UserRepo) {}
 
   async createUser(userData: Omit<User, 'id' | 'created_at'>): Promise<User> {
-    return this.userRepo.create(userData);
+    return await this.userRepo.create(userData);
   }
 
-  async updateUser(id: number, userData: Partial<User>): Promise<User> {
-    return this.userRepo.update(id, userData);
+  async updateUser(id: number, userData: Partial<User>): Promise<void> {
+    await this.userRepo.update(id, userData);
   }
 
   async findUsers(criteria: Partial<User>): Promise<User[]> {
-    return this.userRepo.findBy(criteria);
+    const users = await this.userRepo.findBy(criteria);
+    return users.map((user) => ({
+      ...user,
+      created_at: formatDate(new Date(user.created_at)),
+    }));
   }
 
   async getAllUsers(): Promise<User[]> {
-    return this.userRepo.findAll();
+    const users = await this.userRepo.findAll();
+    return users.map((user) => ({
+      ...user,
+      created_at: formatDate(new Date(user.created_at)),
+    }));
   }
 
   async deleteUser(criteria: Partial<User>): Promise<void> {
